4c8a8aec5fbd0e2657379d85ebe26bb59a0c8c47
FabianUlises <ulisesfab.car@outlook.com>
2023-03-18 18:18:57 -0700

    Added react bootstrap form component

--- src/App.js
+++ src/App.js
@@ -17,6 +17,23 @@ function App() {
         </Nav.Item>
       </Nav>
       <Button variant='success'>Primary</Button>
+      <Form>
+        <Form.Group controlId='formBasicEmail'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type='email' placeholder='Enter email' />
+          <Form.Text className='Text-muted'>
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' placeholder='password' />
+        </Form.Group>
+        <Form.Group controlId='formBasicCheckbox'>
+          <Form.Control type='checkbox' label='check me out' />
+        </Form.Group>
+        <Button variant='primary'>Submit</Button>
+      </Form>
     </div>
   );
 }

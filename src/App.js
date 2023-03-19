5ddcc85a76dd25ae312b902395572aa9e0c13808
FabianUlises <ulisesfab.car@outlook.com>
2023-03-18 18:13:01 -0700

    Exploring react bootstrap components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
-
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Bootstrap Website</h1>
-     
+      <Nav activeKey='/home'>
+        <Nav.Item>
+          <Nav.Link href='/home'>Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href='/about'>About us</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Button variant='success'>Primary</Button>
     </div>
   );
 }
